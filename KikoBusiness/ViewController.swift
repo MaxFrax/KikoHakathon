f6b98bee951516fc2ed560239a78f871fe3c80c5
Massimo Frasson <maxfrax@gmail.com>
2015-05-16 17:13:55 +0200

    + richiesta autorizzazione

--- KikoBusiness/ViewController.swift
+++ KikoBusiness/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //assegno delegato location manager
+        locationManager.delegate = self
+        
+        //chiedo autorizzazione utilizzo posizione utente
+        if(CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedAlways){
+            locationManager.requestAlwaysAuthorization()
+        }
     }
 
     override func didReceiveMemoryWarning() {
